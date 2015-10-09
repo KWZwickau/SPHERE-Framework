2dcc7663532cf036d59a60ba1793722555e8be16
Gerd Christian Kunze <gerdchristian.kunze@googlemail.com>
2015-10-09 18:55:27 +0200

    [Education:Lesson:Term] Improved

--- Application/Education/Lesson/Term/Service/Data.php
+++ Application/Education/Lesson/Term/Service/Data.php
@@ -1,6 +1,10 @@
 <?php
 namespace SPHERE\Application\Education\Lesson\Term\Service;
 
+use SPHERE\Application\Education\Lesson\Term\Service\Entity\TblPeriod;
+use SPHERE\Application\Education\Lesson\Term\Service\Entity\TblYear;
+use SPHERE\Application\Education\Lesson\Term\Service\Entity\TblYearPeriod;
+use SPHERE\Application\Platform\System\Protocol\Protocol;
 use SPHERE\System\Database\Binding\AbstractData;
 
 /**
@@ -14,5 +18,100 @@ class Data extends AbstractData
     public function setupDatabaseContent()
     {
 
+    }
+
+    /**
+     * @param string $Name
+     *
+     * @return TblYear
+     */
+    public function createYear($Name)
+    {
+
+        $Manager = $this->getConnection()->getEntityManager();
+        $Entity = $Manager->getEntity('TblYear')->findOneBy(array(
+            TblYear::ATTR_NAME => $Name
+        ));
+        if (null === $Entity) {
+            $Entity = new TblYear();
+            $Entity->setName($Name);
+            $Manager->saveEntity($Entity);
+            Protocol::useService()->createInsertEntry($this->getConnection()->getDatabase(), $Entity);
+        }
+        return $Entity;
+    }
+
+    /**
+     * @param string $Name
+     * @param string $From
+     * @param string $To
+     *
+     * @return TblPeriod
+     */
+    public function createPeriod($Name, $From, $To)
+    {
+
+        $Manager = $this->getConnection()->getEntityManager();
+        $Entity = $Manager->getEntity('TblPeriod')->findOneBy(array(
+            TblPeriod::ATTR_NAME => $Name
+        ));
+        if (null === $Entity) {
+            $Entity = new TblPeriod();
+            $Entity->setName($Name);
+            $Entity->setFrom(new \DateTime($From));
+            $Entity->setTo(new \DateTime($To));
+            $Manager->saveEntity($Entity);
+            Protocol::useService()->createInsertEntry($this->getConnection()->getDatabase(), $Entity);
+        }
+        return $Entity;
+    }
+
+    /**
+     * @param TblYear   $tblYear
+     * @param TblPeriod $tblPeriod
+     *
+     * @return TblYearPeriod
+     */
+    public function addYearPeriod(TblYear $tblYear, TblPeriod $tblPeriod)
+    {
+
+        $Manager = $this->getConnection()->getEntityManager();
+        $Entity = $Manager->getEntity('TblYearPeriod')
+            ->findOneBy(array(
+                TblYearPeriod::ATTR_TBL_YEAR   => $tblYear->getId(),
+                TblYearPeriod::ATTR_TBL_PERIOD => $tblPeriod->getId()
+            ));
+        if (null === $Entity) {
+            $Entity = new TblYearPeriod();
+            $Entity->setTblYear($tblYear);
+            $Entity->setTblPeriod($tblPeriod);
+            $Manager->saveEntity($Entity);
+            Protocol::useService()->createInsertEntry($this->getConnection()->getDatabase(), $Entity);
+        }
+        return $Entity;
+    }
+
+    /**
+     * @param TblYear   $tblYear
+     * @param TblPeriod $tblPeriod
+     *
+     * @return bool
+     */
+    public function removeYearPeriod(TblYear $tblYear, TblPeriod $tblPeriod)
+    {
+
+        $Manager = $this->getConnection()->getEntityManager();
+        /** @var TblYearPeriod $Entity */
+        $Entity = $Manager->getEntity('TblYearPeriod')
+            ->findOneBy(array(
+                TblYearPeriod::ATTR_TBL_YEAR   => $tblYear->getId(),
+                TblYearPeriod::ATTR_TBL_PERIOD => $tblPeriod->getId()
+            ));
+        if (null !== $Entity) {
+            Protocol::useService()->createDeleteEntry($this->getConnection()->getDatabase(), $Entity);
+            $Manager->killEntity($Entity);
+            return true;
+        }
+        return false;
     }
 }
