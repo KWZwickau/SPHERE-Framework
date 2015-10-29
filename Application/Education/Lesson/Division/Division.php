2eed74aa3e4fcbb728eac7d37ce00e9d5b181d86
Gerd Christian Kunze <gerdchristian.kunze@googlemail.com>
2015-10-29 17:02:19 +0100

    Creating Frontend for Division

--- Application/Education/Lesson/Division/Division.php
+++ Application/Education/Lesson/Division/Division.php
@@ -1,8 +1,19 @@
 <?php
 namespace SPHERE\Application\Education\Lesson\Division;
 
+use SPHERE\Application\Education\Lesson\Division\Service\Entity\TblLevel;
 use SPHERE\Application\IModuleInterface;
 use SPHERE\Application\Platform\Gatekeeper\Authorization\Consumer\Consumer;
+use SPHERE\Common\Frontend\Icon\Repository\Transfer;
+use SPHERE\Common\Frontend\Layout\Repository\Panel;
+use SPHERE\Common\Frontend\Layout\Repository\Title;
+use SPHERE\Common\Frontend\Layout\Structure\Layout;
+use SPHERE\Common\Frontend\Layout\Structure\LayoutColumn;
+use SPHERE\Common\Frontend\Layout\Structure\LayoutGroup;
+use SPHERE\Common\Frontend\Layout\Structure\LayoutRow;
+use SPHERE\Common\Frontend\Link\Repository\Standard;
+use SPHERE\Common\Frontend\Text\Repository\Bold;
+use SPHERE\Common\Frontend\Text\Repository\Italic;
 use SPHERE\Common\Main;
 use SPHERE\Common\Window\Navigation\Link;
 use SPHERE\Common\Window\Stage;
@@ -25,18 +36,12 @@ class Division implements IModuleInterface
         Main::getDispatcher()->registerRoute(Main::getDispatcher()->createRoute(
             __NAMESPACE__, __CLASS__.'::frontendDashboard'
         ));
-    }
-
-    /**
-     * @return Service
-     */
-    public static function useService()
-    {
-
-        return new Service(
-            new Identifier('Education', 'Lesson', 'Division', null, Consumer::useService()->getConsumerBySession()),
-            __DIR__.'/Service/Entity', __NAMESPACE__.'\Service\Entity'
-        );
+        Main::getDispatcher()->registerRoute(Main::getDispatcher()->createRoute(
+            __NAMESPACE__.'/Create/Level', __NAMESPACE__.'\Frontend::frontendCreateLevel'
+        ));
+        Main::getDispatcher()->registerRoute(Main::getDispatcher()->createRoute(
+            __NAMESPACE__.'/Create/Division', __NAMESPACE__.'\Frontend::frontendCreateDivision'
+        ));
     }
 
     /**
@@ -56,6 +61,103 @@ class Division implements IModuleInterface
 
         $Stage = new Stage('Dashboard', 'Klassen');
 
+        $Stage->addButton(new Standard('Klassenstufe bearbeiten', __NAMESPACE__.'\Create\Level'));
+        $Stage->addButton(new Standard('Klassengruppe bearbeiten', __NAMESPACE__.'\Create\Division'));
+
+        $tblLevelAll = $this->useService()->getLevelAll();
+        $Content = array();
+
+        //usort( $tblLevelAll,  );
+
+        if ($tblLevelAll) {
+            array_push($Content, new LayoutRow(array(
+                new LayoutColumn(array(
+                    new Title(new Italic(new Bold('Unzugeordnet'))),
+                ))
+            )));
+
+//        $tblUnusedSubjectAll = $this->useService()->getSubjectAllHavingNoCategory();
+//        if ($tblUnusedSubjectAll) {
+//            array_walk($tblUnusedSubjectAll, function (TblSubject &$tblSubject) {
+//
+//                $tblSubject = new Bold($tblSubject->getAcronym()).' - '
+//                    .$tblSubject->getName().' '
+//                    .new Small(new Muted($tblSubject->getDescription()));
+//            });
+//        } else {
+//            $tblUnusedSubjectAll = new Success('Keine unzugeordneten Klassen');
+//        }
+//        $tblUnusedCategoryAll = $this->useService()->getCategoryAllHavingNoGroup();
+//        if ($tblUnusedCategoryAll) {
+//            array_walk($tblUnusedCategoryAll, function (TblCategory &$tblCategory) {
+//
+//                $tblCategory = new Bold($tblCategory->getName()).' - '
+//                    .new Small(new Muted($tblCategory->getDescription()));
+//            });
+//        } else {
+//            $tblUnusedCategoryAll = new Success('Keine unzugeordneten Kategorien');
+//        }
+
+//        array_push($Content, new LayoutRow(array(
+//            new LayoutColumn(new Panel('Kategorien', $tblUnusedCategoryAll), 6),
+//            new LayoutColumn(new Panel('Klassen', $tblUnusedSubjectAll), 6),
+//        )));
+
+            // Payload
+            array_walk($tblLevelAll, function (TblLevel $tblLevel) use (&$Content) {
+
+                array_push($Content, new LayoutRow(array(
+                    new LayoutColumn(array(
+                        new Title('Klassenstufe: '.new Bold($tblLevel->getName()), $tblLevel->getDescription()),
+                        new Standard('Zuweisen von Kategorien', __NAMESPACE__.'\Link\Category', new Transfer(),
+                            array('Id' => $tblLevel->getId())
+                        )
+                    ))
+                )));
+//            $tblCategoryAll = $this->useService()->getCategoryAllByLevel($tblLevel);
+//            array_walk($tblCategoryAll, function (TblCategory $tblCategory) use (&$Content, $tblLevel) {
+//
+//                $tblSubjectAll = $this->useService()->getSubjectAllByCategory($tblCategory);
+//                array_walk($tblSubjectAll, function (TblSubject &$tblSubject) {
+//
+//                    $tblSubject = new Bold($tblSubject->getAcronym()).' - '
+//                        .$tblSubject->getName().' '
+//                        .new Small(new Muted($tblSubject->getDescription()));
+//                });
+//
+//                $Height = floor(( ( count($tblSubjectAll) + 2 ) / 3 ) + 1);
+                Main::getDispatcher()->registerWidget($tblLevel->getName(),
+                    new Panel(
+//                        $tblCategory->getName().' '.$tblCategory->getDescription(),
+//                        $tblSubjectAll,
+//                        ( $tblCategory->getIsLocked() ? Panel::PANEL_TYPE_INFO : Panel::PANEL_TYPE_DEFAULT ),
+                        '',
+                        new Standard('Zuweisen von Klassen-Gruppen', __NAMESPACE__.'\Link\Subject', new Transfer()
+                        //array('Id' => $tblCategory->getId()
+                        ))
+                );
+                //, 2, ( $Height ? $Height : $Height + 2 ));
+//            });
+                array_push($Content, new LayoutRow(array(
+                    new LayoutColumn(Main::getDispatcher()->fetchDashboard($tblLevel->getName()))
+                )));
+            });
+        }
+        $Stage->setContent(
+            new Layout(new LayoutGroup($Content, new Title('Klassen', 'Zusammensetzung')))
+        );
         return $Stage;
+    }
+
+    /**
+     * @return Service
+     */
+    public static function useService()
+    {
+
+        return new Service(
+            new Identifier('Education', 'Lesson', 'Division', null, Consumer::useService()->getConsumerBySession()),
+            __DIR__.'/Service/Entity', __NAMESPACE__.'\Service\Entity'
+        );
     }
 }
--- Application/Education/Lesson/Division/Service.php
+++ Application/Education/Lesson/Division/Service.php
@@ -2,8 +2,15 @@
 namespace SPHERE\Application\Education\Lesson\Division;
 
 use SPHERE\Application\Education\Lesson\Division\Service\Data;
+use SPHERE\Application\Education\Lesson\Division\Service\Entity\TblDivision;
 use SPHERE\Application\Education\Lesson\Division\Service\Entity\TblLevel;
 use SPHERE\Application\Education\Lesson\Division\Service\Setup;
+use SPHERE\Application\Education\School\Type\Service\Entity\TblType;
+use SPHERE\Application\Education\School\Type\Type;
+use SPHERE\Common\Frontend\Form\IFormInterface;
+use SPHERE\Common\Frontend\Message\Repository\Danger;
+use SPHERE\Common\Frontend\Message\Repository\Success;
+use SPHERE\Common\Window\Redirect;
 use SPHERE\System\Database\Binding\AbstractService;
 
 /**
@@ -48,5 +55,85 @@ class Service extends AbstractService
     {
 
         return (new Data($this->getBinding()))->getLevelById($Id);
+    }
+
+    /**
+     * @return bool|TblDivision[]
+     */
+    public function getDivisionAll()
+    {
+
+        return (new Data($this->getBinding()))->getDivisionAll();
+    }
+
+    /**
+     * @param int $Id
+     *
+     * @return bool|TblDivision
+     */
+    public function getDivisionById($Id)
+    {
+
+        return (new Data($this->getBinding()))->getDivisionById($Id);
+    }
+
+    /**
+     * @param IFormInterface $Form
+     * @param null|array     $Level
+     *
+     * @return IFormInterface|string
+     */
+    public function createLevel(
+        IFormInterface $Form,
+        $Level
+    ) {
+
+        /**
+         * Skip to Frontend
+         */
+        if (null === $Level) {
+            return $Form;
+        }
+
+        $Error = false;
+
+        $tblType = Type::useService()->getTypeById($Level['Type']);
+
+        if (isset( $Level['Name'] ) && empty( $Level['Name'] )) {
+            $Form->setError('Level[Name]', 'Bitte geben Sie einen eineindeutigen Namen in Bezug auf die Schulart an');
+            $Error = true;
+        } else {
+            if ($this->checkLevelExists($tblType, $Level['Name'])) {
+                $Form->setError('Level[Name]', 'Dieser Name wird bereits verwendet');
+                $Error = true;
+            }
+        }
+
+        if (!$Error) {
+
+            if ((new Data($this->getBinding()))->createLevel(
+                $tblType, $Level['Name'], $Level['Description']
+            )
+            ) {
+                return new Success('Die Klassenstufe wurde erfolgreich hinzugefügt')
+                .new Redirect($this->getRequest()->getUrl(), 1);
+            } else {
+                return new Danger('Die Klassenstufe konnte nicht hinzugefügt werden')
+                .new Redirect($this->getRequest()->getUrl());
+            }
+        }
+        return $Form;
+    }
+
+    /**
+     * @param TblType $tblType
+     * @param string  $Name
+     *
+     * @return bool
+     */
+    public function checkLevelExists(TblType $tblType, $Name)
+    {
+
+        return (new Data($this->getBinding()))->checkLevelExists($tblType, $Name);
     }
 }
