d2f6aa65b56b3a45205b09e51e5574486e7a490a
Johannes <hannesk001@web.de>
2015-12-11 15:18:38 +0100

    Transfer -> Import -> Chemnitz import nur für Mandant ESCZ sichtbar

--- Application/Transfer/Import/Chemnitz/Chemnitz.php
+++ Application/Transfer/Import/Chemnitz/Chemnitz.php
@@ -2,8 +2,12 @@
 namespace SPHERE\Application\Transfer\Import\Chemnitz;
 
 use MOC\V\Component\Document\Component\Bridge\Repository\PhpExcel;
+use MOC\V\Core\FileSystem\FileSystem;
 use SPHERE\Application\IModuleInterface;
+use SPHERE\Common\Frontend\Icon\Repository\Upload;
 use SPHERE\Common\Frontend\IFrontendInterface;
+use SPHERE\Common\Frontend\Layout\Repository\Thumbnail;
+use SPHERE\Common\Frontend\Link\Repository\Standard;
 use SPHERE\Common\Main;
 use SPHERE\Common\Window\Navigation\Link;
 
@@ -30,6 +34,28 @@ class Chemnitz implements IModuleInterface
         Main::getDispatcher()->registerRoute(Main::getDispatcher()->createRoute(
             __NAMESPACE__ . '/InterestedPerson', __NAMESPACE__ . '\Frontend::frontendInterestedPersonImport'
         ));
+
+        Main::getDispatcher()->registerWidget('Import',
+            new Thumbnail(
+                FileSystem::getFileLoader('/Common/Style/Resource/eszc.png'),
+                'Chemntiz', 'Schülerdaten',
+                new Standard('', '/Transfer/Import/Chemnitz/Student', new Upload(), array(), 'Upload')
+            ), 2, 2
+        );
+        Main::getDispatcher()->registerWidget('Import',
+            new Thumbnail(
+                FileSystem::getFileLoader('/Common/Style/Resource/eszc.png'),
+                'Chemntiz', 'Interessentendaten',
+                new Standard('', '/Transfer/Import/Chemnitz/InterestedPerson', new Upload(), array(), 'Upload')
+            ), 2, 2
+        );
+        Main::getDispatcher()->registerWidget('Import',
+            new Thumbnail(
+                FileSystem::getFileLoader('/Common/Style/Resource/eszc.png'),
+                'Chemntiz', 'Personendaten',
+                new Standard('', '/Transfer/Import/Chemnitz/Person', new Upload(), array(), 'Upload')
+            ), 2, 2
+        );
     }
 
     /**
