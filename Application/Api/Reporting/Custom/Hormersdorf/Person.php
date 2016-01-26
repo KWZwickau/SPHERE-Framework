85125c65d0fe7b7958cc7c40766b57384b00e8b3
Johannes <hannesk001@web.de>
2016-01-26 09:14:18 +0100

    Reporting --> Hormersdorf--> Klassenliste

--- Application/Api/Reporting/Custom/Hormersdorf/Person.php
+++ Application/Api/Reporting/Custom/Hormersdorf/Person.php
@@ -9,6 +9,7 @@
 namespace SPHERE\Application\Api\Reporting\Custom\Hormersdorf;
 
 use MOC\V\Core\FileSystem\FileSystem;
+use SPHERE\Application\Education\Lesson\Division\Division;
 use SPHERE\Application\Reporting\Custom\Hormersdorf\Person\Person as HormersdorfPerson;
 
 /**
@@ -32,6 +33,29 @@ class Person
 
             return FileSystem::getDownload($fileLocation->getRealPath(),
                 "Mitarbeiterliste (Geburtstage) ".date("Y-m-d H:i:s").".xls")->__toString();
+        }
+
+        return false;
+    }
+
+    /**
+     * @param null $DivisionId
+     *
+     * @return string|bool
+     */
+    public function downloadClassList($DivisionId = null)
+    {
+
+        $tblDivision = Division::useService()->getDivisionById($DivisionId);
+        if ($tblDivision) {
+            $studentList = HormersdorfPerson::useService()->createClassList($tblDivision);
+            if ($studentList) {
+                $fileLocation = HormersdorfPerson::useService()->createClassListExcel($studentList);
+
+                return FileSystem::getDownload($fileLocation->getRealPath(),
+                    "Klassenliste ".$tblDivision->getDisplayName()
+                    ." ".date("Y-m-d H:i:s").".xls")->__toString();
+            }
         }
 
         return false;
--- Application/Reporting/Custom/Hormersdorf/Person/Frontend.php
+++ Application/Reporting/Custom/Hormersdorf/Person/Frontend.php
@@ -8,8 +8,22 @@
 
 namespace SPHERE\Application\Reporting\Custom\Hormersdorf\Person;
 
+use SPHERE\Application\Education\Lesson\Division\Division;
+use SPHERE\Application\Education\Lesson\Division\Service\Entity\TblDivision;
+use SPHERE\Common\Frontend\Form\Repository\Field\SelectBox;
+use SPHERE\Common\Frontend\Form\Structure\Form;
+use SPHERE\Common\Frontend\Form\Structure\FormColumn;
+use SPHERE\Common\Frontend\Form\Structure\FormGroup;
+use SPHERE\Common\Frontend\Form\Structure\FormRow;
 use SPHERE\Common\Frontend\Icon\Repository\Download;
+use SPHERE\Common\Frontend\Icon\Repository\Select;
 use SPHERE\Common\Frontend\IFrontendInterface;
+use SPHERE\Common\Frontend\Layout\Repository\Panel;
+use SPHERE\Common\Frontend\Layout\Repository\Well;
+use SPHERE\Common\Frontend\Layout\Structure\Layout;
+use SPHERE\Common\Frontend\Layout\Structure\LayoutColumn;
+use SPHERE\Common\Frontend\Layout\Structure\LayoutGroup;
+use SPHERE\Common\Frontend\Layout\Structure\LayoutRow;
 use SPHERE\Common\Frontend\Link\Repository\Primary;
 use SPHERE\Common\Frontend\Table\Structure\TableData;
 use SPHERE\Common\Window\Stage;
@@ -31,7 +45,7 @@ class Frontend extends Extension implements IFrontendInterface
 
         $View = new Stage();
         $View->setTitle('Auswertung');
-        $View->setDescription('Bitte wählen Sie eine Liste zur Auswertung');
+        $View->setDescription('Bitte wÃ¤hlen Sie eine Liste zur Auswertung');
 
         return $View;
     }
@@ -79,74 +93,70 @@ class Frontend extends Extension implements IFrontendInterface
     {
 
         $View = new Stage();
-        $View->setTitle('ESZC Auswertung');
+        $View->setTitle('Auswertung');
         $View->setDescription('Klassenliste');
 
-//        $tblDivisionAll = Division::useService()->getDivisionAll();
-//        $tblDivision = new TblDivision();
-//        $studentList = array();
-//
-//        if ($DivisionId !== null) {
-//
-//            $Global = $this->getGlobal();
-//            if (!$Global->POST) {
-//                $Global->POST['Select']['Division'] = $DivisionId;
-//                $Global->savePost();
-//            }
-//
-//            //ToDo JohK Schuljahr
-//
-//            $tblDivision = Division::useService()->getDivisionById($DivisionId);
-//            if ($tblDivision) {
-//                $studentList = Person::useService()->createClassList($tblDivision);
-//                if ($studentList) {
-//                    $View->addButton(
-//                        new Primary('Herunterladen',
-//                            '/Api/Reporting/Custom/Chemnitz/Common/ClassList/Download', new Download(),
-//                            array('DivisionId' => $tblDivision->getId()))
-//                    );
-//                }
-//            }
-//        }
-//
-//        $View->setContent(
-//            new Well(
-//                Person::useService()->getClass(
-//                    new Form(new FormGroup(array(
-//                        new FormRow(array(
-//                            new FormColumn(
-//                                new SelectBox('Select[Division]', 'Klasse', array(
-//                                    '{{ serviceTblYear.Name }} - {{ tblLevel.serviceTblType.Name }} - {{ tblLevel.Name }}{{ Name }}' => $tblDivisionAll
-//                                )), 12
-//                            )
-//                        )),
-//                    )), new \SPHERE\Common\Frontend\Form\Repository\Button\Primary('Auswählen', new Select()))
-//                    , $Select, '/Reporting/Custom/Chemnitz/Person/ClassList')
-//            )
-//            .
-//            ( $DivisionId !== null ?
-//                (new Layout(new LayoutGroup(new LayoutRow(array(
-//                    new LayoutColumn(
-//                        new Panel('Klasse:', $tblDivision->getDisplayName(),
-//                            Panel::PANEL_TYPE_INFO), 12
-//                    ),
-//                )))))
-//                .
-//                new TableData($studentList, null,
-//                    array(
-//                        'Salutation'   => 'Anrede',
-//                        'Father'       => 'Vorname Sorgeberechtigter 1',
-//                        'Mother'       => 'Vorname Sorgeberechtigter 2',
-//                        'LastName'     => 'Name',
-//                        'Denomination' => 'Konfession',
-//                        'Address'      => 'Adresse',
-//                        'FirstName'    => 'Schüler',
-//                        'Birthday'     => 'Geburtsdatum',
-//                        'Birthplace'   => 'Geburtsort',
-//                    ),
-//                    null
-//                ) : '' )
-//        );
+        $tblDivisionAll = Division::useService()->getDivisionAll();
+        $tblDivision = new TblDivision();
+        $studentList = array();
+
+        if ($DivisionId !== null) {
+
+            $Global = $this->getGlobal();
+            if (!$Global->POST) {
+                $Global->POST['Select']['Division'] = $DivisionId;
+                $Global->savePost();
+            }
+
+            $tblDivision = Division::useService()->getDivisionById($DivisionId);
+            if ($tblDivision) {
+                $studentList = Person::useService()->createClassList($tblDivision);
+                if ($studentList) {
+                    $View->addButton(
+                        new Primary('Herunterladen',
+                            '/Api/Reporting/Custom/Hormersdorf/Person/ClassList/Download',
+                            new Download(),
+                            array('DivisionId' => $tblDivision->getId()))
+                    );
+                }
+            }
+        }
+
+        $tableData = ($tableData = new TableData($studentList, null,
+            array(
+                'DisplayName' => 'Name',
+                'Birthday' => 'Geb.-Datum',
+                'Address' => 'Adresse',
+                'PhoneNumbers' => 'Telefonnummer',
+            ),
+            null
+        ));
+
+        $View->setContent(
+            new Well(
+                Person::useService()->getClass(
+                    new Form(new FormGroup(array(
+                        new FormRow(array(
+                            new FormColumn(
+                                new SelectBox('Select[Division]', 'Klasse', array(
+                                    '{{ serviceTblYear.Name }} - {{ tblLevel.serviceTblType.Name }} - {{ tblLevel.Name }}{{ Name }}' => $tblDivisionAll
+                                )), 12
+                            )
+                        )),
+                    )), new \SPHERE\Common\Frontend\Form\Repository\Button\Primary('AuswÃ¤hlen', new Select()))
+                    , $Select, '/Reporting/Custom/Hormersdorf/Person/ClassList')
+            )
+            .
+            ($DivisionId !== null ?
+                (new Layout(new LayoutGroup(new LayoutRow(array(
+                    new LayoutColumn(
+                        new Panel('Klasse:', $tblDivision->getDisplayName(),
+                            Panel::PANEL_TYPE_INFO), 12
+                    ),
+                )))))
+                . $tableData
+                : '')
+        );
 
         return $View;
     }
