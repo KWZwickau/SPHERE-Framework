27cca93e4fa062531242160407a8102ef690afce
Johannes <hannesk001@web.de>
2016-01-27 14:56:55 +0100

    Icons hinzugefügt

---
+++ Common/Frontend/Icon/Repository/Family.php
@@ -0,0 +1,43 @@
+<?php
+namespace SPHERE\Common\Frontend\Icon\Repository;
+
+use SPHERE\Common\Frontend\Icon\IIconInterface;
+
+/**
+ * Class Family
+ *
+ * @package SPHERE\Common\Frontend\Icon\Repository
+ */
+class Family implements IIconInterface
+{
+
+    /** @var string $Value */
+    private $Value = 'glyphicons glyphicons-family';
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+
+        return $this->getContent();
+    }
+
+    /**
+     * @return string
+     */
+    public function getContent()
+    {
+
+        return '<span class="'.$this->getValue().'" aria-hidden="true"></span>';
+    }
+
+    /**
+     * @return string
+     */
+    public function getValue()
+    {
+
+        return $this->Value;
+    }
+}
