4f3ce61dc0df502acb133bea1cd3bc9cedd17510
Gerd Christian Kunze <gerdchristian.kunze@googlemail.com>
2016-03-31 16:33:18 +0200

    Fix: #KREDA-334

--- Application/Corporation/Company/Service/Entity/TblCompany.php
+++ Application/Corporation/Company/Service/Entity/TblCompany.php
@@ -5,6 +5,8 @@ use Doctrine\ORM\Mapping\Cache;
 use Doctrine\ORM\Mapping\Column;
 use Doctrine\ORM\Mapping\Entity;
 use Doctrine\ORM\Mapping\Table;
+use SPHERE\Application\Contact\Address\Address;
+use SPHERE\Application\Contact\Address\Service\Entity\TblAddress;
 use SPHERE\Application\Corporation\Group\Group;
 use SPHERE\Application\Corporation\Group\Service\Entity\TblGroup;
 use SPHERE\System\Database\Fitting\Element;
@@ -101,5 +103,14 @@ class TblCompany extends Element
     {
 
         return $this->Name.( $this->ExtendedName != '' ? ' '.$this->ExtendedName : null );
+    }
+    
+    /**
+     * @return bool|TblAddress
+     */
+    public function fetchMainAddress()
+    {
+
+        return Address::useService()->getAddressByCompany($this);
     }
 }
