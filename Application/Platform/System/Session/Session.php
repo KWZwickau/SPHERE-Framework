55bb8375ed8ed6a0ea86e70fec549d5c98c2acb1
Gerd Christian Kunze <gerdchristian.kunze@googlemail.com>
2016-04-06 17:49:45 +0200

    Enable: Table DateTime Sorting

--- Application/Platform/System/Session/Session.php
+++ Application/Platform/System/Session/Session.php
@@ -3,7 +3,6 @@ namespace SPHERE\Application\Platform\System\Session;
 
 use SPHERE\Application\IModuleInterface;
 use SPHERE\Application\IServiceInterface;
-use SPHERE\Application\People\Person\Service\Entity\TblPerson;
 use SPHERE\Application\Platform\Gatekeeper\Authorization\Account\Account;
 use SPHERE\Application\Platform\Gatekeeper\Authorization\Account\Service\Entity\TblSession;
 use SPHERE\Application\Platform\System\Protocol\Protocol;
@@ -16,9 +15,7 @@ use SPHERE\Common\Frontend\Layout\Structure\LayoutColumn;
 use SPHERE\Common\Frontend\Layout\Structure\LayoutGroup;
 use SPHERE\Common\Frontend\Layout\Structure\LayoutRow;
 use SPHERE\Common\Frontend\Table\Structure\TableData;
-use SPHERE\Common\Frontend\Text\Repository\Italic;
 use SPHERE\Common\Frontend\Text\Repository\Muted;
-use SPHERE\Common\Frontend\Text\Repository\Small;
 use SPHERE\Common\Main;
 use SPHERE\Common\Window\Navigation\Link;
 use SPHERE\Common\Window\Redirect;
@@ -104,25 +101,10 @@ class Session extends Extension implements IModuleInterface
         if ($tblProtocolAll) {
             array_walk($tblProtocolAll, function (TblProtocol $tblProtocol) use (&$History) {
 
-                $tblAccount = $tblProtocol->getServiceTblAccount();
-                /** @var null|TblPerson|TblPerson[] $tblPerson */
-                $tblPerson = null;
-                if ($tblAccount) {
-                    $tblPerson = Account::useService()->getPersonAllByAccount($tblAccount);
-                    if (!empty( $tblPerson )) {
-                        $tblPerson = current($tblPerson);
-                    } else {
-                        $tblPerson = null;
-                    }
-                }
-
                 array_push($History, array(
                     'Consumer'  => $tblProtocol->getConsumerAcronym().'&nbsp;'.new Muted($tblProtocol->getConsumerName()),
                     'LoginTime' => $tblProtocol->getEntityCreate(),
-                    'Account'   =>
-                        new Muted('Account: ').$tblProtocol->getConsumerAcronym().'-'.$tblProtocol->getAccountUsername()
-                        .new Muted('&nbsp;&nbsp;Person: ').( $tblPerson ? ' '.$tblPerson->getFullName() : new Small(new Italic('-NA-')) )
-                ,
+                    'Account'   => $tblProtocol->getAccountUsername()
                 ));
 
             });
@@ -156,7 +138,7 @@ class Session extends Extension implements IModuleInterface
                             ), array(
                                 'order'      => array(array(0, 'desc')),
                                 'columnDefs' => array(
-                                    array('width' => '10%', 'targets' => 0),
+                                    array('type' => 'de_datetime', 'width' => '10%', 'targets' => 0),
                                     array('width' => '45%', 'targets' => 1),
                                     array('width' => '45%', 'targets' => 2)
                                 )
