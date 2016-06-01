640ef276dacdbf27c3bf1446d25fd0183351273a
Johannes <hannesk001@web.de>
2016-06-01 13:18:12 +0200

    KREDA-478 Personen Zuweisung zu Gruppen per Massenverfahren

--- Application/People/Group/Frontend.php
+++ Application/People/Group/Frontend.php
@@ -1,10 +1,15 @@
 <?php
 namespace SPHERE\Application\People\Group;
 
+use SPHERE\Application\Education\Lesson\Division\Division;
+use SPHERE\Application\Education\Lesson\Term\Term;
 use SPHERE\Application\People\Group\Service\Entity\TblGroup;
+use SPHERE\Application\People\Meta\Student\Student;
 use SPHERE\Application\People\Person\Person as PeoplePerson;
 use SPHERE\Application\People\Person\Service\Entity\TblPerson;
 use SPHERE\Common\Frontend\Form\Repository\Button\Primary;
+use SPHERE\Common\Frontend\Form\Repository\Field\CheckBox;
+use SPHERE\Common\Frontend\Form\Repository\Field\SelectBox;
 use SPHERE\Common\Frontend\Form\Repository\Field\TextArea;
 use SPHERE\Common\Frontend\Form\Repository\Field\TextField;
 use SPHERE\Common\Frontend\Form\Structure\Form;
@@ -15,9 +20,12 @@ use SPHERE\Common\Frontend\Icon\Repository\Ban;
 use SPHERE\Common\Frontend\Icon\Repository\ChevronLeft;
 use SPHERE\Common\Frontend\Icon\Repository\Disable;
 use SPHERE\Common\Frontend\Icon\Repository\Edit;
+use SPHERE\Common\Frontend\Icon\Repository\Exclamation;
+use SPHERE\Common\Frontend\Icon\Repository\Filter;
 use SPHERE\Common\Frontend\Icon\Repository\ListingTable;
 use SPHERE\Common\Frontend\Icon\Repository\Ok;
 use SPHERE\Common\Frontend\Icon\Repository\Pencil;
+use SPHERE\Common\Frontend\Icon\Repository\PersonGroup;
 use SPHERE\Common\Frontend\Icon\Repository\PlusSign;
 use SPHERE\Common\Frontend\Icon\Repository\Question;
 use SPHERE\Common\Frontend\Icon\Repository\Remove;
@@ -39,6 +47,7 @@ use SPHERE\Common\Frontend\Link\Repository\Standard;
 use SPHERE\Common\Frontend\Message\Repository\Danger;
 use SPHERE\Common\Frontend\Message\Repository\Success;
 use SPHERE\Common\Frontend\Message\Repository\Warning;
+use SPHERE\Common\Frontend\Table\Structure\TableData;
 use SPHERE\Common\Frontend\Text\Repository\Bold;
 use SPHERE\Common\Frontend\Text\Repository\Muted;
 use SPHERE\Common\Frontend\Text\Repository\Small;
@@ -63,7 +72,7 @@ class Frontend extends Extension implements IFrontendInterface
     {
 
         $Stage = new Stage('Gruppen', 'Übersicht');
-        $Stage->addButton( new Backward() );
+        $Stage->addButton(new Backward());
 
         $tblGroupAll = Group::useService()->getGroupAll();
         if ($tblGroupAll) {
@@ -78,6 +87,12 @@ class Frontend extends Extension implements IFrontendInterface
                 $Footer = new PullLeft(
                     new Standard('', '/People/Group/Edit', new Edit(),
                         array('Id' => $tblGroup->getId()), 'Daten ändern'
+                    )
+                    . ($tblGroup->getMetaTable() !== 'COMMON'
+                        ? new Standard('', '/People/Group/Person/Add', new PersonGroup(),
+                            array('Id' => $tblGroup->getId()), 'Personen zuweisen'
+                        )
+                        : ''
                     )
                     . ($tblGroup->isLocked()
                         ? ''
@@ -175,7 +190,7 @@ class Frontend extends Extension implements IFrontendInterface
     {
 
         $Stage = new Stage('Gruppe', 'Bearbeiten');
-        $Stage->addButton( new Backward(true) );
+        $Stage->addButton(new Backward(true));
 
         $tblGroup = Group::useService()->getGroupById($Id);
         if ($tblGroup) {
@@ -246,7 +261,7 @@ class Frontend extends Extension implements IFrontendInterface
     {
 
         $Stage = new Stage('Gruppe', 'Löschen');
-        $Stage->addButton( new Backward(true) );
+        $Stage->addButton(new Backward(true));
 
         if ($Id) {
             $tblGroup = Group::useService()->getGroupById($Id);
@@ -304,5 +319,257 @@ class Frontend extends Extension implements IFrontendInterface
             );
         }
         return $Stage;
+    }
+
+    /**
+     * @param null $Id
+     * @param null $Data
+     * @param null $Filter
+     * @param null $FilterGroupId
+     * @param null $FilterDivisionId
+     *
+     * @return Stage
+     */
+    public function frontendGroupPersonAdd(
+        $Id = null,
+        $Data = null,
+        $Filter = null,
+        $FilterGroupId = null,
+        $FilterDivisionId = null
+    ) {
+
+        $Stage = new Stage('Gruppe', 'Personen zuweisen');
+        $Stage->addButton(new Standard('Zurück', '/People/Group', new ChevronLeft()));
+
+        if (($tblGroup = Group::useService()->getGroupById($Id))) {
+
+            $tblFilterGroup = Group::useService()->getGroupById($FilterGroupId);
+            $tblFilterDivision = Division::useService()->getDivisionById($FilterDivisionId);
+
+            // Set Filter Post
+            if ($Filter == null && ($tblFilterGroup || $tblFilterDivision)) {
+                $GLOBAL = $this->getGlobal();
+                $GLOBAL->POST['Filter']['Group'] = $tblFilterGroup ? $tblFilterGroup->getId() : 0;
+                $GLOBAL->POST['Filter']['Division'] = $tblFilterDivision ? $tblFilterDivision->getId() : 0;
+
+                $GLOBAL->savePost();
+            }
+
+            $tblPersonList = Group::useService()->getPersonAllByGroup($tblGroup);
+            $tblPersonAll = Group::useService()->getPersonAllByGroup(Group::useService()->getGroupByMetaTable('COMMON'));
+
+            if ($tblFilterGroup || $tblFilterDivision) {
+                $tblPersonAll = Group::useService()->filterPersonListByGroupAndDivision(
+                    $tblPersonAll,
+                    $tblFilterGroup ? $tblFilterGroup : null,
+                    $tblFilterDivision ? $tblFilterDivision : null
+                );
+            }
+
+            if ($tblPersonList && $tblPersonAll) {
+                $tblPersonAll = array_udiff($tblPersonAll, $tblPersonList,
+                    function (TblPerson $tblPersonA, TblPerson $tblPersonB) {
+
+                        return $tblPersonA->getId() - $tblPersonB->getId();
+                    }
+                );
+            }
+
+            if ($tblPersonList) {
+                if ($Data === null) {
+                    $GLOBAL = $this->getGlobal();
+                    foreach ($tblPersonList as $tblPerson) {
+                        $GLOBAL->POST['Data'][$tblPerson->getId()] = 1;
+                    }
+                    $GLOBAL->savePost();
+                }
+
+                $tempList = array();
+                foreach ($tblPersonList as $personListPerson) {
+                    $tempList[] = $this->setPersonData($personListPerson);
+                }
+                $tblPersonList = $tempList;
+            }
+
+            if (is_array($tblPersonAll)) {
+                $tempList = array();
+                foreach ($tblPersonAll as $personAllPerson) {
+                    $tempList[] = $this->setPersonData($personAllPerson);
+                }
+                $tblPersonAll = $tempList;
+            }
+
+            $form = new Form(array(
+                new FormGroup(
+                    new FormRow(array(
+                        new FormColumn(array(
+                            ($tblPersonList
+                                ? new TableData(
+                                    $tblPersonList,
+                                    null,
+                                    array(
+                                        'Check' => '',
+                                        'DisplayName' => 'Name',
+                                        'Address' => 'Addresse',
+                                        'Groups' => 'Gruppen/Klasse'
+                                    ),
+                                    array(
+                                        'order' => array(
+                                            array('1', 'asc')
+                                        ),
+                                        "paging" => false, // Deaktivieren Blättern
+                                        "iDisplayLength" => -1,    // Alle Einträge zeigen
+                                        "searching" => false, // Deaktivieren Suchen
+                                        "info" => false  // Deaktivieren Such-Info
+                                    )
+                                )
+                                : new Warning('Keine Personen zugewiesen.', new Exclamation())
+                            )
+                        ), 6),
+                    )), new \SPHERE\Common\Frontend\Form\Repository\Title('Ausgewählte', 'Personen')),
+                new FormGroup(
+                    new FormRow(array(
+                        new FormColumn(array(
+                            ($tblPersonAll
+                                ? new TableData(
+                                    $tblPersonAll,
+                                    null,
+                                    array(
+                                        'Check' => '',
+                                        'DisplayName' => 'Name',
+                                        'Address' => 'Addresse',
+                                        'Groups' => 'Gruppen/Klasse '
+                                    ),
+                                    array(
+                                        'order' => array(
+                                            array('1', 'asc')
+                                        ),
+                                        "paging" => false, // Deaktivieren Blättern
+                                        "iDisplayLength" => -1,    // Alle Einträge zeigen
+                                    )
+                                )
+                                : new Warning('Keine weiteren Personen verfügbar.', new Exclamation())
+                            )
+                        ), 6)
+                    )), new \SPHERE\Common\Frontend\Form\Repository\Title('Ausgewählte', 'Personen')
+                )
+            ));
+
+            $form->appendFormButton(new Primary('Speichern', new Save()));
+
+            $Stage->setContent(new Layout(array(
+                new LayoutGroup(
+                    new LayoutRow(
+                        new LayoutColumn(
+                            new Panel(
+                                'Gruppe',
+                                $tblGroup->getName() . ' ' . new Small(new Muted($tblGroup->getDescription())),
+                                Panel::PANEL_TYPE_INFO
+                            )
+                        )
+                    )
+                ),
+                new LayoutGroup(
+                    new LayoutRow(array(
+                        new LayoutColumn(
+                            '&nbsp;', 6
+                        ),
+                        new LayoutColumn(
+                            new Well(
+                                Group::useService()->getFilter(
+                                    $this->formFilter(), $tblGroup, $Filter
+                                )
+                            ), 6
+                        )
+                    ))
+                    , new Title(new Filter() . ' Filter', 'Verfügbare Personen')),
+                ($Filter == null ?
+                    new LayoutGroup(
+                        new LayoutRow(array(
+                            new LayoutColumn(array(
+                                Group::useService()->addPersonsToGroup($form, $tblGroup, $Data,
+                                    $tblFilterGroup ? $tblFilterGroup : null,
+                                    $tblFilterDivision ? $tblFilterDivision : null
+                                )
+                            ))
+                        ))
+                    ) : null)
+            )));
+
+        } else {
+            return $Stage
+            . new Danger('Gruppe nicht gefunden.', new Ban())
+            . new Redirect('/People/Group', Redirect::TIMEOUT_ERROR);
+        }
+
+        return $Stage;
+    }
+
+    /**
+     * @param TblPerson $tblPerson
+     * @return array
+     */
+    private function setPersonData(TblPerson $tblPerson)
+    {
+        $result = array();
+        $result['Check'] = new CheckBox(
+            'Data[' . $tblPerson->getId() . ']',
+            '&nbsp;&nbsp;&nbsp;',
+            1
+        );
+        $result['DisplayName'] = $tblPerson->getLastFirstName();
+        $tblAddress = $tblPerson->fetchMainAddress();
+        $result['Address'] = $tblAddress ? $tblAddress->getGuiString() : '';
+        $tblGroupList = Group::useService()->getGroupAllByPerson($tblPerson);
+        $groups = array();
+        if ($tblGroupList) {
+            foreach ($tblGroupList as $item) {
+                if ($item->getMetaTable() !== 'COMMON') {
+                    $groups[] = $item->getName();
+                }
+            }
+        }
+
+        // current Division
+        $tblDivision = Student::useService()->getCurrentDivisionByPerson($tblPerson);
+
+        $result['Groups'] = (!empty($groups) ? implode(', ', $groups) . ($tblDivision ? ', ' : '') : '')
+            . ($tblDivision ? 'Klasse ' . $tblDivision->getDisplayName() : '');
+
+        return $result;
+    }
+
+    private function formFilter()
+    {
+
+        $tblGroupAll = Group::useService()->getGroupAllSorted();
+        $tblDivisionList = array();
+        $tblYearList = Term::useService()->getYearByNow();
+        if ($tblYearList) {
+            foreach ($tblYearList as $tblYear) {
+                $tblDivisionAllByYear = Division::useService()->getDivisionByYear($tblYear);
+                if ($tblDivisionAllByYear) {
+                    foreach ($tblDivisionAllByYear as $tblDivision) {
+                        $tblDivisionList[$tblDivision->getId()] = $tblDivision;
+                    }
+                }
+            }
+        }
+
+        return new Form(
+            new FormGroup(
+                new FormRow(array(
+                    new FormColumn(
+                        new SelectBox('Filter[Group]', 'Gruppe', array('Name' => $tblGroupAll)), 6
+                    ),
+                    new FormColumn(
+                        new SelectBox('Filter[Division]', 'Klasse', array('DisplayName' => $tblDivisionList)), 6
+                    ),
+                    new FormColumn(
+                        new Primary('Filtern', new Filter())
+                    ),
+                ))
+            )
+        );
     }
 }
