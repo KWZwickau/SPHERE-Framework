66ed96fe8407ff3377cb479c807fa141aa1af064
Kauschke <hannesk001@web.de>
2016-09-30 12:52:43 +0200

    #KREDA-701 Datenimport für 	Ev. Schulverein Rochlitzer Land e.V. ESRL --> Mitarbeiterimport

---
+++ Application/Transfer/Import/Seelitz/Frontend.php
@@ -0,0 +1,162 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kauschke
+ * Date: 30.09.2016
+ * Time: 09:32
+ */
+
+namespace SPHERE\Application\Transfer\Import\Seelitz;
+
+use SPHERE\Common\Frontend\Form\Repository\Button\Primary;
+use SPHERE\Common\Frontend\Form\Repository\Field\FileUpload;
+use SPHERE\Common\Frontend\Form\Structure\Form;
+use SPHERE\Common\Frontend\Form\Structure\FormColumn;
+use SPHERE\Common\Frontend\Form\Structure\FormGroup;
+use SPHERE\Common\Frontend\Form\Structure\FormRow;
+use SPHERE\Common\Frontend\Icon\Repository\Exclamation;
+use SPHERE\Common\Frontend\IFrontendInterface;
+use SPHERE\Common\Frontend\Layout\Repository\Well;
+use SPHERE\Common\Frontend\Layout\Structure\Layout;
+use SPHERE\Common\Frontend\Layout\Structure\LayoutColumn;
+use SPHERE\Common\Frontend\Layout\Structure\LayoutGroup;
+use SPHERE\Common\Frontend\Layout\Structure\LayoutRow;
+use SPHERE\Common\Frontend\Text\Repository\Warning;
+use SPHERE\Common\Window\Stage;
+use SPHERE\System\Extension\Extension;
+
+/**
+ * Class Frontend
+ *
+ * @package SPHERE\Application\Transfer\Import\Seelitz
+ */
+class Frontend extends Extension implements IFrontendInterface
+{
+
+    /**
+     * @param null $File
+     *
+     * @return Stage
+     *
+     * @throws \MOC\V\Component\Document\Exception\DocumentTypeException
+     */
+    public function frontendStudentImport($File = null)
+    {
+
+        $View = new Stage('Import Seelitz', 'Schüler-Daten');
+        $View->setContent(
+            new Layout(
+                new LayoutGroup(
+                    new LayoutRow(
+                        new LayoutColumn(array(
+                            new Well(
+                                Seelitz::useService()->createStudentsFromFile(new Form(
+                                    new FormGroup(
+                                        new FormRow(
+                                            new FormColumn(
+                                                new FileUpload('File', 'Datei auswählen', 'Datei auswählen', null,
+                                                    array('showPreview' => false))
+                                            )
+                                        )
+                                    )
+                                    , new Primary('Hochladen')
+                                ), $File
+                                )
+                                ,
+                                new Warning('Erlaubte Dateitypen: Excel (XLS,XLSX)', new Exclamation())
+                            )
+                        ))
+                    )
+                )
+            )
+        );
+
+        return $View;
+    }
+
+    /**
+     * @param null $File
+     *
+     * @return Stage
+     *
+     * @throws \MOC\V\Component\Document\Exception\DocumentTypeException
+     */
+    public function frontendStaffImport($File = null)
+    {
+
+        $View = new Stage();
+        $View->setTitle('Seelitz Import');
+        $View->setDescription('Mitarbeiter/Lehrer');
+        $View->setContent(
+            new Layout(
+                new LayoutGroup(
+                    new LayoutRow(
+                        new LayoutColumn(array(
+                            new Well(
+                                Seelitz::useService()->createStaffsFromFile(new Form(
+                                    new FormGroup(
+                                        new FormRow(
+                                            new FormColumn(
+                                                new FileUpload('File', 'Datei auswählen', 'Datei auswählen', null,
+                                                    array('showPreview' => false))
+                                            )
+                                        )
+                                    )
+                                    , new Primary('Hochladen')
+                                ), $File
+                                )
+                                ,
+                                new Warning('Erlaubte Dateitypen: Excel (XLS,XLSX)')
+                            )
+                        ))
+                    )
+                )
+            )
+        );
+
+        return $View;
+    }
+
+    /**
+     * @param null $File
+     *
+     * @return Stage
+     *
+     * @throws \MOC\V\Component\Document\Exception\DocumentTypeException
+     */
+    public function frontendInterestedPersonImport($File = null)
+    {
+
+        $View = new Stage();
+        $View->setTitle('Seelitz Import');
+        $View->setDescription('Interessentendaten');
+        $View->setContent(
+            new Layout(
+                new LayoutGroup(
+                    new LayoutRow(
+                        new LayoutColumn(array(
+                            new Well(
+                                Seelitz::useService()->createInterestedPersonsFromFile(new Form(
+                                    new FormGroup(
+                                        new FormRow(
+                                            new FormColumn(
+                                                new FileUpload('File', 'Datei auswählen', 'Datei auswählen', null,
+                                                    array('showPreview' => false))
+                                            )
+                                        )
+                                    )
+                                    , new Primary('Hochladen')
+                                ), $File
+                                )
+                                ,
+                                new Warning('Erlaubte Dateitypen: Excel (XLS,XLSX)', new Exclamation())
+                            )
+                        ))
+                    )
+                )
+            )
+        );
+
+        return $View;
+    }
+}
---
+++ Application/Transfer/Import/Seelitz/Seelitz.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kauschke
+ * Date: 30.09.2016
+ * Time: 09:33
+ */
+
+namespace SPHERE\Application\Transfer\Import\Seelitz;
+
+use MOC\V\Core\FileSystem\FileSystem;
+use SPHERE\Application\IModuleInterface;
+use SPHERE\Common\Frontend\Icon\Repository\Upload;
+use SPHERE\Common\Frontend\Layout\Repository\Thumbnail;
+use SPHERE\Common\Frontend\Link\Repository\Standard;
+use SPHERE\Common\Main;
+
+/**
+ * Class Seelitz
+ *
+ * @package SPHERE\Application\Transfer\Import\Seelitz
+ */
+class Seelitz implements IModuleInterface
+{
+
+    public static function registerModule()
+    {
+
+        Main::getDispatcher()->registerRoute(Main::getDispatcher()->createRoute(
+            __NAMESPACE__ . '/Student', __NAMESPACE__ . '\Frontend::frontendStudentImport'
+        ));
+        Main::getDispatcher()->registerRoute(Main::getDispatcher()->createRoute(
+            __NAMESPACE__ . '/Staff', __NAMESPACE__ . '\Frontend::frontendStaffImport'
+        ));
+        Main::getDispatcher()->registerRoute(Main::getDispatcher()->createRoute(
+            __NAMESPACE__ . '/InterestedPerson', __NAMESPACE__ . '\Frontend::frontendInterestedPersonImport'
+        ));
+
+        Main::getDispatcher()->registerWidget('Import', array(__CLASS__, 'widgetStaff'), 2, 2);
+        Main::getDispatcher()->registerWidget('Import', array(__CLASS__, 'widgetStudent'), 2, 2);
+        Main::getDispatcher()->registerWidget('Import', array(__CLASS__, 'widgetInterestedPerson'), 2, 2);
+    }
+
+    /**
+     * @return Service
+     */
+    public static function useService()
+    {
+
+        return new Service();
+    }
+
+    /**
+     * @return Frontend
+     */
+    public static function useFrontend()
+    {
+
+        return new Frontend();
+    }
+
+    /**
+     * @return Thumbnail
+     */
+    public static function widgetStudent()
+    {
+
+        return new Thumbnail(
+            FileSystem::getFileLoader('/Common/Style/Resource/logo_kreide2.png'),
+            'Seelitz', 'Schüler-Daten',
+            new Standard('', '/Transfer/Import/Seelitz/Student', new Upload(), array(), 'Upload')
+        );
+    }
+
+    /**
+     * @return Thumbnail
+     */
+    public static function widgetStaff()
+    {
+
+        return new Thumbnail(
+            FileSystem::getFileLoader('/Common/Style/Resource/logo_kreide2.png'),
+            'Seelitz', 'Mitarbeiter/Lehrer',
+            new Standard('', '/Transfer/Import/Seelitz/Staff', new Upload(), array(), 'Upload')
+        );
+    }
+
+
+    /**
+     * @return Thumbnail
+     */
+    public static function widgetInterestedPerson()
+    {
+
+        return new Thumbnail(
+            FileSystem::getFileLoader('/Common/Style/Resource/logo_kreide2.png'),
+            'Seelitz', 'Interessenten',
+            new Standard('', '/Transfer/Import/Seelitz/InterestedPerson', new Upload(), array(), 'Upload')
+        );
+    }
+}
