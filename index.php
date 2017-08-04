0ee037117371241eb0a3be33faaced857f248f47
BozzaCoon <siddy_88@hotmail.de>
2017-08-04 12:52:13 +0200

    reactivate Cache

--- index.php
+++ index.php
@@ -46,7 +46,7 @@ if (false) {
 }
 
 // Clear Cache
-if (true) {
+if (false) {
     (new CacheFactory())->createHandler(new CookieHandler())->clearCache();
     (new CacheFactory())->createHandler(new MemcachedHandler())->clearCache();
     (new CacheFactory())->createHandler(new APCuHandler())->clearCache();
