b31c364b285a135142676c8c1aa0c4e40783771d
Kauschke <hannesk001@web.de>
2018-05-29 08:05:36 +0200

    fix: SSW-2275 Fehlerbehebung Adresslisten für Serienbriefe

--- Application/Reporting/SerialLetter/Service.php
+++ Application/Reporting/SerialLetter/Service.php
@@ -572,7 +572,7 @@ class Service extends AbstractService
         }
         $isCompany = false;
         $FilterCategory = SerialLetter::useService()->getFilterCategoryByName(TblFilterCategory::IDENTIFIER_COMPANY_GROUP);
-        if (( $tblFilterCategory = $tblSerialLetter->getFilterCategory() )) {
+        if ($FilterCategory && ( $tblFilterCategory = $tblSerialLetter->getFilterCategory() )) {
             if ($FilterCategory->getId() == $tblFilterCategory->getId()) {
                 $isCompany = true;
             }
