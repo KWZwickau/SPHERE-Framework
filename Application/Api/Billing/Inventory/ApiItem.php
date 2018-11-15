a9c0c6fe33307b785f41c309bd32f7f416346a16
BozzaCoon <siddy_88@hotmail.de>
2018-11-15 09:31:56 +0100

    SSW-350 überarbeitung der Eingabemaske im Modal, Fehlermeldungen nun wieder an den Elementen möglich; Erweiterung der Funktionen

--- Application/Api/Billing/Inventory/ApiItem.php
+++ Application/Api/Billing/Inventory/ApiItem.php
@@ -4,15 +4,25 @@ namespace SPHERE\Application\Api\Billing\Inventory;
 use SPHERE\Application\Api\ApiTrait;
 use SPHERE\Application\Api\Dispatcher;
 use SPHERE\Application\Billing\Inventory\Item\Item;
+use SPHERE\Application\Billing\Inventory\Setting\Setting;
 use SPHERE\Application\IApiInterface;
 use SPHERE\Application\People\Group\Group;
+use SPHERE\Application\People\Group\Service\Entity\TblGroup;
 use SPHERE\Common\Frontend\Ajax\Emitter\ServerEmitter;
 use SPHERE\Common\Frontend\Ajax\Pipeline;
+use SPHERE\Common\Frontend\Ajax\Receiver\BlockReceiver;
 use SPHERE\Common\Frontend\Ajax\Receiver\ModalReceiver;
 use SPHERE\Common\Frontend\Ajax\Template\CloseModal;
 use SPHERE\Common\Frontend\Form\Repository\Button\Close;
+use SPHERE\Common\Frontend\Form\Repository\Field\CheckBox;
+use SPHERE\Common\Frontend\Form\Repository\Field\TextField;
+use SPHERE\Common\Frontend\Form\Structure\Form;
+use SPHERE\Common\Frontend\Form\Structure\FormColumn;
+use SPHERE\Common\Frontend\Form\Structure\FormGroup;
+use SPHERE\Common\Frontend\Form\Structure\FormRow;
 use SPHERE\Common\Frontend\Icon\Repository\Disable;
 use SPHERE\Common\Frontend\Icon\Repository\Ok;
+use SPHERE\Common\Frontend\Icon\Repository\Save;
 use SPHERE\Common\Frontend\Layout\Repository\Listing;
 use SPHERE\Common\Frontend\Layout\Repository\Panel;
 use SPHERE\Common\Frontend\Layout\Structure\Layout;
@@ -20,6 +30,7 @@ use SPHERE\Common\Frontend\Layout\Structure\LayoutColumn;
 use SPHERE\Common\Frontend\Layout\Structure\LayoutGroup;
 use SPHERE\Common\Frontend\Layout\Structure\LayoutRow;
 use SPHERE\Common\Frontend\Link\Repository\Danger as DangerLink;
+use SPHERE\Common\Frontend\Link\Repository\Primary;
 use SPHERE\Common\Frontend\Message\Repository\Danger;
 use SPHERE\Common\Frontend\Message\Repository\Success;
 use SPHERE\Common\Frontend\Message\Repository\Warning;
@@ -45,9 +56,12 @@ class ApiItem extends Extension implements IApiInterface
     {
         $Dispatcher = new Dispatcher(__CLASS__);
         $Dispatcher->registerMethod('showAddItem');
-        $Dispatcher->registerMethod('addItem');
-        $Dispatcher->registerMethod('editItem');
+        $Dispatcher->registerMethod('saveAddItem');
+        $Dispatcher->registerMethod('saveEditItem');
         $Dispatcher->registerMethod('showEditItem');
+        // reload Frontend Info
+        $Dispatcher->registerMethod('getItemTable');
+
         $Dispatcher->registerMethod('showAddVariant');
         $Dispatcher->registerMethod('showDeleteItem');
         $Dispatcher->registerMethod('deleteItem');
@@ -70,14 +84,23 @@ class ApiItem extends Extension implements IApiInterface
     }
 
     /**
+     * @param string $Content
+     * @return BlockReceiver
+     */
+    public static function receiverItemTable($Content = '')
+    {
+
+        return (new BlockReceiver($Content))->setIdentifier('BlockTableContent');
+    }
+
+    /**
      * @param string $Identifier
-     * @param array  $Warning
      * @param array  $Item
      * @param array  $Group
      *
      * @return Pipeline
      */
-    public static function pipelineOpenAddItemModal($Identifier = '', $Warning = array(), $Item = array(), $Group = array())
+    public static function pipelineOpenAddItemModal($Identifier = '', $Item = array(), $Group = array())
     {
 
         $Receiver = self::receiverModal(null, $Identifier);
@@ -88,7 +111,6 @@ class ApiItem extends Extension implements IApiInterface
         ));
         $Emitter->setPostPayload(array(
             'Identifier' => $Identifier,
-            'Warning' => $Warning,
             'Item' => $Item,
             'Group' => $Group
         ));
@@ -109,15 +131,40 @@ class ApiItem extends Extension implements IApiInterface
         $Pipeline = new Pipeline();
         $Emitter = new ServerEmitter($Receiver, ApiItem::getEndpoint());
         $Emitter->setGetPayload(array(
-            ApiItem::API_TARGET => 'addItem'
+            ApiItem::API_TARGET => 'saveAddItem'
         ));
         $Emitter->setPostPayload(array(
             'Identifier' => $Identifier
         ));
         $Pipeline->appendEmitter($Emitter);
-        // Close Modal
-        $Pipeline->appendEmitter((new CloseModal(self::receiverModal('', $Identifier)))->getEmitter());
-        //ToDO reload Table
+
+        return $Pipeline;
+    }
+
+    /**
+     * @param string     $Identifier
+     * @param int|string $ItemId
+     * @param array      $Item
+     * @param array      $Group
+     *
+     * @return Pipeline
+     */
+    public static function pipelineOpenEditItemModal($Identifier = '', $ItemId = '',$Item = array(), $Group = array())
+    {
+
+        $Receiver = self::receiverModal(null, $Identifier);
+        $Pipeline = new Pipeline(true);
+        $Emitter = new ServerEmitter($Receiver, ApiItem::getEndpoint());
+        $Emitter->setGetPayload(array(
+            ApiItem::API_TARGET => 'showEditItem'
+        ));
+        $Emitter->setPostPayload(array(
+            'Identifier' => $Identifier,
+            'ItemId'     => $ItemId,
+            'Item'       => $Item,
+            'Group'      => $Group,
+        ));
+        $Pipeline->appendEmitter($Emitter);
 
         return $Pipeline;
     }
@@ -135,44 +182,11 @@ class ApiItem extends Extension implements IApiInterface
         $Pipeline = new Pipeline(true);
         $Emitter = new ServerEmitter($Receiver, ApiItem::getEndpoint());
         $Emitter->setGetPayload(array(
-            ApiItem::API_TARGET => 'editItem'
+            ApiItem::API_TARGET => 'saveEditItem'
         ));
         $Emitter->setPostPayload(array(
             'Identifier' => $Identifier,
             'ItemId' => $ItemId
-        ));
-        $Pipeline->appendEmitter($Emitter);
-        // Close Modal
-        $Pipeline->appendEmitter((new CloseModal(self::receiverModal('', $Identifier)))->getEmitter());
-        //ToDO reload Table
-
-        return $Pipeline;
-    }
-
-    /**
-     * @param string     $Identifier
-     * @param int|string $ItemId
-     * @param array      $Warning
-     * @param array      $Item
-     * @param array      $Group
-     *
-     * @return Pipeline
-     */
-    public static function pipelineOpenEditItemModal($Identifier = '', $ItemId = '', $Warning = array(), $Item = array(), $Group = array())
-    {
-
-        $Receiver = self::receiverModal(null, $Identifier);
-        $Pipeline = new Pipeline(true);
-        $Emitter = new ServerEmitter($Receiver, ApiItem::getEndpoint());
-        $Emitter->setGetPayload(array(
-            ApiItem::API_TARGET => 'showEditItem'
-        ));
-        $Emitter->setPostPayload(array(
-            'Identifier' => $Identifier,
-            'ItemId'     => $ItemId,
-            'Warning'    => $Warning,
-            'Item'       => $Item,
-            'Group'      => $Group,
         ));
         $Pipeline->appendEmitter($Emitter);
 
@@ -245,9 +259,134 @@ class ApiItem extends Extension implements IApiInterface
             'ItemId'     => $ItemId,
         ));
         $Pipeline->appendEmitter($Emitter);
-        //ToDO reload Table
 
         return $Pipeline;
+    }
+
+    /**
+     * @param string $Identifier
+     *
+     * @return Pipeline
+     */
+    public static function pipelineCloseModal($Identifier = '')
+    {
+        $Pipeline = new Pipeline();
+        // reload the whole Table
+        $Emitter = new ServerEmitter(self::receiverItemTable(''), self::getEndpoint());
+        $Emitter->setGetPayload(array(
+            self::API_TARGET => 'getItemTable'
+        ));
+        $Pipeline->appendEmitter($Emitter);
+        $Pipeline->appendEmitter((new CloseModal(self::receiverModal('', $Identifier)))->getEmitter());
+        return $Pipeline;
+    }
+
+    public function getItemTable()
+    {
+
+        return Item::useFrontend()->getItemTable();
+    }
+
+    /**
+     * @param string     $Identifier
+     * @param int|string $ItemId
+     *
+     * @return Form
+     */
+    public function formItem($Identifier = '', $ItemId = '')
+    {
+
+        $CheckboxList = array();
+        $tblGroupList = array();
+
+        // choose between Add and Edit
+        $SaveButton =new Primary('Speichern', ApiItem::getEndpoint(), new Save());
+        if('' !== $ItemId /* && ($tblItem = Item::useService()->getItemById($ItemId)) */){
+            $SaveButton->ajaxPipelineOnClick(ApiItem::pipelineSaveEditItem($Identifier, $ItemId));
+        } else {
+            $SaveButton->ajaxPipelineOnClick(ApiItem::pipelineSaveAddItem($Identifier));
+        }
+
+        // get all possible Person Groups
+        if(($tblSettingGroupPersonAll = Setting::useService()->getSettingGroupPersonAll())){
+            foreach($tblSettingGroupPersonAll as $tblSettingGroupPerson){
+                if(($tblGroup = $tblSettingGroupPerson->getServiceTblGroupPerson())){
+                    $tblGroupList[] = $tblGroup;
+                }
+            }
+        }
+        if(($tblGroupList = $this->getSorter($tblGroupList)->sortObjectBy('Name'))){
+            /** @var TblGroup $tblGroup */
+            foreach($tblGroupList as $tblGroup){
+                $CheckboxList[] = new CheckBox('Group['.$tblGroup->getId().']', $tblGroup->getName(), $tblGroup->getId());
+            }
+        }
+
+
+        return (new Form(
+            new FormGroup(
+                new FormRow(array(
+                    new FormColumn(
+                        (new TextField('Item[Name]', 'Beitragsart', 'Beitragsart'))->setRequired()
+                        , 6),
+                    new FormColumn(
+                        $CheckboxList
+                        , 6),
+//                    new FormColumn(
+//                        new TextField('Item[Description]', 'Beschreibung', 'Beschreibung')
+//                    , 6),
+                    new FormColumn(
+                        $SaveButton
+                    )
+                ))
+            )
+        ))->disableSubmitAction();
+    }
+
+    /**
+     * @param string $Identifier
+     * @param string $ItemId
+     * @param array $Item
+     * @param array $Group
+     *
+     * @return false|string|Form
+     */
+    private function checkInputItem($Identifier = '', $ItemId = '',$Item = array(), $Group = array())
+    {
+        $Error = false;
+        $form = $this->formItem($Identifier, $ItemId);
+        $Warning = '';
+        if (isset($Item['Name']) && empty($Item['Name'])) {
+            $form->setError('Item[Name]', 'Bitte geben Sie den Namen der Beitragsart an');
+            $Error = true;
+            // disable save for duplicated names
+        } elseif(isset($Item['Name']) && ($tblItem = Item::useService()->getItemByName($Item['Name']))){
+            // ignore own Name
+            if($tblItem->getId() != $ItemId){
+                $form->setError('Item[Name]','Beitragsart exisitiert bereits, sie darf nicht doppelt angelegt werden');
+                $Error = true;
+            }
+        }
+        if (empty($Group)) {
+            $Warning = 'Bitte geben Sie mindestens eine Personengruppe an';
+            $form->setError('Group[1]', 'Bitte geben Sie eine Hausnummer an');
+            $Error = true;
+        }
+        if(empty($Group)){
+
+            $Error = true;
+        }
+
+
+        if ($Error) {
+            if($Warning){
+                return new Danger($Warning).$form;
+            } else {
+                return $form;
+            }
+        }
+
+        return $Error;
     }
 
     /**
@@ -256,17 +395,10 @@ class ApiItem extends Extension implements IApiInterface
      *
      * @return string
      */
-    public function showAddItem($Identifier = '', $Warning = array())
+    public function showAddItem($Identifier = '')
     {
 
-        if(!empty($Warning)){
-            foreach($Warning as &$Entry){
-                $Entry = new Danger($Entry);
-            }
-        }
-
-        return implode($Warning)
-            .Item::useFrontend()->formItem($Identifier);
+        return $this->formItem($Identifier);
     }
 
     /**
@@ -276,25 +408,17 @@ class ApiItem extends Extension implements IApiInterface
      *
      * @return string
      */
-    public function addItem($Identifier = '', $Item = array(), $Group = array())
+    public function saveAddItem($Identifier = '', $Item = array(), $Group = array())
     {
 
-        $Error = false;
-        $Warning = array();
-        if($Item['Name'] === ''){
-            $Warning[] = 'Bitte geben Sie den Namen der Beitragsart an';
-            $Error = true;
-        } elseif(Item::useService()->getItemByName($Item['Name'])){
-            $Warning[] = 'Beitragsart exisitiert bereits, sie darf nicht doppelt angelegt werden';
-            $Error = true;
-        }
-        if(empty($Group)){
-
-            $Warning[] = 'Bitte geben Sie mindestens eine Personengruppe an';
-            $Error = true;
-        }
-        if($Error){
-            return self::pipelineOpenAddItemModal($Identifier, $Warning, $Item, $Group);
+        // Handle error's
+        if ($form = $this->checkInputItem($Identifier, '', $Item, $Group)) {
+            // display Errors on form
+            $Global = $this->getGlobal();
+            $Global->POST['Item']['Name'] = $Item['Name'];
+            $Global->POST['Group'] = $Group;
+            $Global->savePost();
+            return $form;
         }
 
         if(($tblItem = Item::useService()->createItem($Item))){
@@ -306,7 +430,7 @@ class ApiItem extends Extension implements IApiInterface
         }
 
         return ($Item
-                    ? new Success('Beitragsart erfolgreich angelegt')
+                    ? new Success('Beitragsart erfolgreich angelegt'). self::pipelineCloseModal($Identifier)
                     : new Danger('Beitragsart konnte nicht gengelegt werden'));
     }
 
@@ -318,25 +442,17 @@ class ApiItem extends Extension implements IApiInterface
      *
      * @return string
      */
-    public function editItem($Identifier = '', $ItemId = '',$Item = array(), $Group = array())
+    public function saveEditItem($Identifier = '', $ItemId = '',$Item = array(), $Group = array())
     {
 
-        $Error = false;
-        $Warning = array();
-        if($Item['Name'] === ''){
-            $Warning[] = 'Bitte geben Sie den Namen der Beitragsart an';
-            $Error = true;
-        } elseif(($tblItem = Item::useService()->getItemByName($Item['Name'])) && $tblItem->getId() != $ItemId){
-            $Warning[] = 'Beitragsart exisitiert bereits, sie darf nicht doppelt angelegt werden';
-            $Error = true;
-        }
-        if(empty($Group)){
-
-            $Warning[] = 'Bitte geben Sie mindestens eine Personengruppe an';
-            $Error = true;
-        }
-        if($Error){
-            return self::pipelineOpenEditItemModal($Identifier, $ItemId, $Warning, $Item, $Group);
+        // Handle error's
+        if ($form = $this->checkInputItem($Identifier, $ItemId, $Item, $Group)) {
+            // display Errors on form
+            $Global = $this->getGlobal();
+            $Global->POST['Item']['Name'] = $Item['Name'];
+            $Global->POST['Group'] = $Group;
+            $Global->savePost();
+            return $form;
         }
 
         if(($tblItem = Item::useService()->getItemById($ItemId))){
@@ -358,28 +474,33 @@ class ApiItem extends Extension implements IApiInterface
         }
 
         return ($Item
-                    ? new Success('Beitragsart erfolgreich angelegt')
+                    ? new Success('Beitragsart erfolgreich angelegt') . self::pipelineCloseModal($Identifier)
                     : new Danger('Beitragsart konnte nicht gengelegt werden'));
     }
 
     /**
      * @param string     $Identifier
      * @param int|string $ItemId
-     * @param array      $Warning
      *
      * @return string
      */
-    public function showEditItem($Identifier = '', $ItemId = '', $Warning = array())
+    public function showEditItem($Identifier = '', $ItemId = '')
     {
 
-        if(!empty($Warning)){
-            foreach($Warning as &$Entry){
-                $Entry = new Danger($Entry);
+        if('' !== $ItemId && ($tblItem = Item::useService()->getItemById($ItemId))){
+            $Global = $this->getGlobal();
+            $Global->POST['Item']['Name'] = $tblItem->getName();
+            if(($tblItemGroupList = Item::useService()->getItemGroupByItem($tblItem))){
+                foreach($tblItemGroupList as $tblItemGroup){
+                    if(($tblGroup = $tblItemGroup->getServiceTblGroup())){
+                        $Global->POST['Group'][$tblGroup->getId()] = $tblGroup->getId();
+                    }
+                }
             }
+            $Global->savePost();
         }
 
-        return implode($Warning)
-            .Item::useFrontend()->formItem($Identifier, $ItemId);
+        return self::formItem($Identifier, $ItemId);
     }
 
     /**
@@ -435,13 +556,18 @@ class ApiItem extends Extension implements IApiInterface
         }
     }
 
-    public function deleteItem($ItemId = '')
+    /**
+     * @param string $Identifier
+     * @param string $ItemId
+     * @return string
+     */
+    public function deleteItem($Identifier = '', $ItemId = '')
     {
 
         if(($tblItem = Item::useService()->getItemById($ItemId))){
             Item::useService()->removeItem($tblItem);
 
-            return new Success('Beitragsart wurde erfolgreich entfernt');
+            return new Success('Beitragsart wurde erfolgreich entfernt'). self::pipelineCloseModal($Identifier);
         }
         return new Danger('Beitragsart konnte nicht entfernt werden');
     }
--- Application/Billing/Inventory/Item/Frontend.php
+++ Application/Billing/Inventory/Item/Frontend.php
@@ -4,19 +4,10 @@ namespace SPHERE\Application\Billing\Inventory\Item;
 
 use SPHERE\Application\Api\Billing\Inventory\ApiItem;
 use SPHERE\Application\Billing\Inventory\Item\Service\Entity\TblItem;
-use SPHERE\Application\Billing\Inventory\Setting\Setting;
-use SPHERE\Application\People\Group\Service\Entity\TblGroup;
-use SPHERE\Common\Frontend\Form\Repository\Field\CheckBox;
-use SPHERE\Common\Frontend\Form\Repository\Field\TextField;
-use SPHERE\Common\Frontend\Form\Structure\Form;
-use SPHERE\Common\Frontend\Form\Structure\FormColumn;
-use SPHERE\Common\Frontend\Form\Structure\FormGroup;
-use SPHERE\Common\Frontend\Form\Structure\FormRow;
 use SPHERE\Common\Frontend\Icon\Repository\Edit;
 use SPHERE\Common\Frontend\Icon\Repository\ListingTable;
 use SPHERE\Common\Frontend\Icon\Repository\Plus;
 use SPHERE\Common\Frontend\Icon\Repository\Remove;
-use SPHERE\Common\Frontend\Icon\Repository\Save;
 use SPHERE\Common\Frontend\IFrontendInterface;
 use SPHERE\Common\Frontend\Layout\Repository\Title;
 use SPHERE\Common\Frontend\Layout\Structure\Layout;
@@ -46,41 +37,6 @@ class Frontend extends Extension implements IFrontendInterface
         $Stage->addButton((new Primary('Beitragsart hinzufügen', ApiItem::getEndpoint(), new Plus()))
             ->ajaxPipelineOnClick(ApiItem::pipelineOpenAddItemModal('AddItem')));
 
-        //ToDO Table as receiver Content
-        $tblItemAll = Item::useService()->getItemAll();
-        $TableContent = array();
-        if (!empty( $tblItemAll )) {
-            array_walk($tblItemAll, function (TblItem $tblItem) use (&$TableContent) {
-
-                $Item['Name'] = $tblItem->getName();
-                $Item['PersonGroup'] = '';
-//                $Item['ItemType'] = $tblItem->getTblItemType()->getName();
-                $Item['Variant'] = '';
-
-                $Item['Option'] =
-                    (new Standard('', ApiItem::getEndpoint(), new Edit(), array(), 'Bearbeiten'))
-                    ->ajaxPipelineOnClick(ApiItem::pipelineOpenEditItemModal('EditItem', $tblItem->getId()))
-                    .(new Standard('', ApiItem::getEndpoint(), new Plus(), array(), 'Varianten hinzufügen'))
-                        ->ajaxPipelineOnClick(ApiItem::pipelineOpenAddVariantModal('AddVariant'))
-                    .(new Standard('', ApiItem::getEndpoint(), new Remove(), array(), 'Löschen'))
-                        ->ajaxPipelineOnClick(ApiItem::pipelineOpenDeleteItemModal('deleteItem', $tblItem->getId()));
-
-                $GroupList = array();
-                if(($PersonGroupList = Item::useService()->getItemGroupByItem($tblItem))){
-                    foreach($PersonGroupList as $PersonGroup){
-                        if(($tblGroup = $PersonGroup->getServiceTblGroup())){
-                            $GroupList[] = $tblGroup->getName();
-                        }
-                    }
-                }
-                if(!empty($GroupList)){
-                    $Item['PersonGroup'] = implode(', ', $GroupList);
-                }
-
-                array_push($TableContent, $Item);
-            });
-        }
-
         $Stage->setContent(
             ApiItem::receiverModal('Anlegen einer neuen Beitragsart', 'AddItem')
             .ApiItem::receiverModal('Beitragsart bearbeiten', 'EditItem')
@@ -90,14 +46,7 @@ class Frontend extends Extension implements IFrontendInterface
                 new LayoutGroup(array(
                     new LayoutRow(
                         new LayoutColumn(
-                            new TableData($TableContent, null,
-                                array(
-                                    'Name'        => 'Name',
-                                    'PersonGroup' => 'zugewiesene Personengruppen',
-                                    'Variant'     => 'Preis-Varianten',
-                                    'Option'      => ''
-                                )
-                            )
+                            ApiItem::receiverItemTable($this->getItemTable())
                         )
                     ), new Title(new ListingTable().' Übersicht')
                 ))
@@ -108,67 +57,53 @@ class Frontend extends Extension implements IFrontendInterface
     }
 
     /**
-     * @param string     $Identifier
-     * @param int|string $ItemId
-     *
-     * @return Form
+     * @return TableData
      */
-    public function formItem($Identifier = '', $ItemId = '')
+    public function getItemTable()
     {
 
-        $CheckboxList = array();
-        $tblGroupList = array();
+        $tblItemAll = Item::useService()->getItemAll();
+        $TableContent = array();
+        if (!empty($tblItemAll)) {
+            array_walk($tblItemAll, function (TblItem $tblItem) use (&$TableContent) {
 
-        if('' !== $ItemId && ($tblItem = Item::useService()->getItemById($ItemId))){
-            $Global = $this->getGlobal();
-            $Global->POST['Item']['Name'] = $tblItem->getName();
-            if(($tblItemGroupList = Item::useService()->getItemGroupByItem($tblItem))){
-                foreach($tblItemGroupList as $tblItemGroup){
-                    if(($tblGroup = $tblItemGroup->getServiceTblGroup())){
-                        $Global->POST['Group'][$tblGroup->getId()] = $tblGroup->getId();
+                $Item['Name'] = $tblItem->getName();
+                $Item['PersonGroup'] = '';
+//                $Item['ItemType'] = $tblItem->getTblItemType()->getName();
+                $Item['Variant'] = '';
+
+                $Item['Option'] =
+                    (new Standard('', ApiItem::getEndpoint(), new Edit(), array(), 'Bearbeiten'))
+                        ->ajaxPipelineOnClick(ApiItem::pipelineOpenEditItemModal('EditItem', $tblItem->getId()))
+                    . (new Standard('', ApiItem::getEndpoint(), new Plus(), array(), 'Varianten hinzufügen'))
+                        ->ajaxPipelineOnClick(ApiItem::pipelineOpenAddVariantModal('AddVariant'))
+                    . (new Standard('', ApiItem::getEndpoint(), new Remove(), array(), 'Löschen'))
+                        ->ajaxPipelineOnClick(ApiItem::pipelineOpenDeleteItemModal('deleteItem', $tblItem->getId()));
+
+                $GroupList = array();
+                if (($PersonGroupList = Item::useService()->getItemGroupByItem($tblItem))) {
+                    foreach ($PersonGroupList as $PersonGroup) {
+                        if (($tblGroup = $PersonGroup->getServiceTblGroup())) {
+                            $GroupList[] = $tblGroup->getName();
+                        }
                     }
+                    sort($GroupList);
                 }
-            }
-            $Global->savePost();
-            $SaveButton =(new Primary('Speichern', ApiItem::getEndpoint(), new Save()))
-                ->ajaxPipelineOnClick(ApiItem::pipelineSaveEditItem($Identifier, $ItemId));
-        } else {
-            $SaveButton =(new Primary('Speichern', ApiItem::getEndpoint(), new Save()))
-                ->ajaxPipelineOnClick(ApiItem::pipelineSaveAddItem($Identifier));
-        }
-
-        if(($tblSettingGroupPersonAll = Setting::useService()->getSettingGroupPersonAll())){
-            foreach($tblSettingGroupPersonAll as $tblSettingGroupPerson){
-                if(($tblGroup = $tblSettingGroupPerson->getServiceTblGroupPerson())){
-                    $tblGroupList[] = $tblGroup;
+                if (!empty($GroupList)) {
+                    $Item['PersonGroup'] = implode(', ', $GroupList);
                 }
-            }
-        }
-        if(($tblGroupList = $this->getSorter($tblGroupList)->sortObjectBy('Name'))){
-            /** @var TblGroup $tblGroup */
-            foreach($tblGroupList as $tblGroup){
-                $CheckboxList[] = new CheckBox('Group['.$tblGroup->getId().']', $tblGroup->getName(), $tblGroup->getId());
-            }
+
+                array_push($TableContent, $Item);
+            });
         }
 
-
-        return (new Form(
-            new FormGroup(
-                new FormRow(array(
-                    new FormColumn(
-                        (new TextField('Item[Name]', 'Beitragsart', 'Beitragsart'))->setRequired()
-                    , 6),
-                    new FormColumn(
-                        $CheckboxList
-                    , 6),
-//                    new FormColumn(
-//                        new TextField('Item[Description]', 'Beschreibung', 'Beschreibung')
-//                    , 6),
-                    new FormColumn(
-                        $SaveButton
-                )
-                ))
+        return new TableData($TableContent, null,
+            array(
+                'Name'        => 'Name',
+                'PersonGroup' => 'zugewiesene Personengruppen',
+                'Variant'     => 'Preis-Varianten',
+                'Option'      => ''
             )
-        ))->disableSubmitAction();
+        );
     }
 }
