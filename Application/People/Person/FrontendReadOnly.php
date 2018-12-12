bcda4424254b7c5cde40163b8fdae15a1befca98
Kauschke <hannesk001@web.de>
2018-12-12 11:37:00 +0100

    SSW-81 ReadOnly Ansicht aller Personendaten -> Kontakt-Daten

--- Application/People/Person/FrontendReadOnly.php
+++ Application/People/Person/FrontendReadOnly.php
@@ -3,16 +3,28 @@
 namespace SPHERE\Application\People\Person;
 
 use SPHERE\Application\Api\People\Person\ApiPersonReadOnly;
+use SPHERE\Application\Contact\Address\Address;
+use SPHERE\Application\Contact\Mail\Mail;
+use SPHERE\Application\Contact\Phone\Phone;
 use SPHERE\Application\Education\Lesson\Division\Filter\Service as FilterService;
+use SPHERE\Application\People\Person\Service\Entity\TblPerson;
+use SPHERE\Application\People\Relationship\Relationship;
+use SPHERE\Common\Frontend\Icon\Repository\ChevronDown;
 use SPHERE\Common\Frontend\Icon\Repository\ChevronLeft;
 use SPHERE\Common\Frontend\Icon\Repository\Info;
+use SPHERE\Common\Frontend\Icon\Repository\TagList;
 use SPHERE\Common\Frontend\IFrontendInterface;
+use SPHERE\Common\Frontend\Layout\Repository\Title;
+use SPHERE\Common\Frontend\Layout\Structure\Layout;
 use SPHERE\Common\Frontend\Layout\Structure\LayoutColumn;
+use SPHERE\Common\Frontend\Layout\Structure\LayoutGroup;
+use SPHERE\Common\Frontend\Layout\Structure\LayoutRow;
 use SPHERE\Common\Frontend\Link\Repository\Standard;
 use SPHERE\Common\Frontend\Message\Repository\Danger;
-use SPHERE\Common\Frontend\Text\Repository\Bold;
-use SPHERE\Common\Window\Stage;
 use SPHERE\Common\Frontend\Message\Repository\Success as SuccessMessage;
+use SPHERE\Common\Frontend\Text\Repository\Bold;
+use SPHERE\Common\Frontend\Text\Repository\Success as SuccessText;
+use SPHERE\Common\Window\Stage;
 use SPHERE\System\Extension\Extension;
 
 /**
@@ -86,10 +98,71 @@ class FrontendReadOnly extends Extension implements IFrontendInterface
                 . $custodyContent
                 . $clubContent
                 . $integrationContent
+                . self::getLayoutContact($tblPerson, $Group)
             );
         }
 
         return $stage;
+    }
+
+    /**
+     * @param TblPerson $tblPerson
+     * @param $Group
+     *
+     * @return Layout
+     */
+    private static function getLayoutContact(TblPerson $tblPerson, $Group)
+    {
+
+        return new Layout(array(
+            new LayoutGroup(array(
+                new LayoutRow(new LayoutColumn(
+                    Address::useFrontend()->frontendLayoutPerson($tblPerson, $Group)
+                )),
+            ), (new Title(new TagList().' Adressdaten',
+                'der Person '.new Bold(new SuccessText($tblPerson->getFullName()))))
+                ->addButton(
+                    new Standard('Adresse hinzufügen', '/People/Person/Address/Create',
+                        new ChevronDown(), array('Id' => $tblPerson->getId(), 'Group' => $Group)
+                    )
+                )
+            ),
+            new LayoutGroup(array(
+                new LayoutRow(new LayoutColumn(
+                    Phone::useFrontend()->frontendLayoutPerson($tblPerson , $Group)
+                    . Mail::useFrontend()->frontendLayoutPerson($tblPerson, $Group)
+                )),
+            ), (new Title(new TagList().' Kontaktdaten',
+                'der Person '.new Bold(new SuccessText($tblPerson->getFullName()))))
+                ->addButton(
+                    new Standard('Telefonnummer hinzufügen', '/People/Person/Phone/Create',
+                        new ChevronDown(), array('Id' => $tblPerson->getId(), 'Group' => $Group)
+                    )
+                )
+                ->addButton(
+                    new Standard('E-Mail Adresse hinzufügen', '/People/Person/Mail/Create',
+                        new ChevronDown(), array('Id' => $tblPerson->getId(), 'Group' => $Group)
+                    )
+                )
+            ),
+            new LayoutGroup(array(
+                new LayoutRow(new LayoutColumn(array(
+                    Relationship::useFrontend()->frontendLayoutPerson($tblPerson, $Group),
+                    Relationship::useFrontend()->frontendLayoutCompany($tblPerson, $Group)
+                ))),
+            ), ( new Title(new TagList().' Beziehungen', new Bold(new SuccessText($tblPerson->getFullName())).' zu Personen und Institutionen') )
+                ->addButton(
+                    new Standard('Personenbeziehung hinzufügen', '/People/Person/Relationship/Create',
+                        new ChevronDown(), array('Id' => $tblPerson->getId(), 'Group' => $Group)
+                    )
+                )
+                ->addButton(
+                    new Standard('Institutionenbeziehung hinzufügen', '/Corporation/Company/Relationship/Create',
+                        new ChevronDown(), array('Id' => $tblPerson->getId(), 'Group' => $Group)
+                    )
+                )
+            )
+        ));
     }
 
     /**
