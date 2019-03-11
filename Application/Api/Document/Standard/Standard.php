13474559f35a1cdf6941bb9d64a833f1d05f0914
Johannes <hannesk001@web.de>
2019-03-11 09:07:14 +0100

    fix: SSW-381 Überarbeitung Erstellung Elternzugänge (PDF-Merge)

--- Application/Api/Document/Standard/Standard.php
+++ Application/Api/Document/Standard/Standard.php
@@ -189,7 +189,8 @@ class Standard extends Extension implements IModuleInterface
                 $Post
             );
         }
-        return Creator::createDataPdf($Data, 'MultiPassword', Creator::PAPERORIENTATION_PORTRAIT);
+
+        return Creator::createMultiPasswordPdf($Data, Creator::PAPERORIENTATION_PORTRAIT);
     }
 
     /**
