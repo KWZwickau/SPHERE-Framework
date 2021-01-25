5ef36d5826aa86ce551127f1003c45d5187806ea
Kauschke <hannesk001@web.de>
2021-01-25 15:05:38 +0100

    fix: SSW-1234 Anpassung individuelle Zeugnisse für Bad Düben

--- Application/Api/Education/Certificate/Generator/Repository/ESBD/EsbdMsHjInfoFsLernen.php
+++ Application/Api/Education/Certificate/Generator/Repository/ESBD/EsbdMsHjInfoFsLernen.php
@@ -73,7 +73,7 @@ class EsbdMsHjInfoFsLernen extends EsbdStyle
             ->addSlice($this->getDateLineConsumer($personId))
             ->addSlice($this->getSignPartConsumer($personId, false))
             ->addSlice($this->getParentSignConsumer())
-            ->addSlice($this->getInfoConsumer('30px',
+            ->addSlice($this->getInfoConsumer('10px',
                 'Notenerläuterung:',
                 '1 = sehr gut; 2 = gut; 3 = befriedigend; 4 = ausreichend; 5 = mangelhaft; 6 = ungenügend 
                     (6 = ungenügend nur bei der Bewertung der Leistungen)',
