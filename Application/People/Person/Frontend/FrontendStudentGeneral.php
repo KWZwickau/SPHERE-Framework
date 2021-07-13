c081c84d217b7e9e4295a9c493b1dd35d6fd1e23
Kauschke <hannesk001@web.de>
2021-07-13 11:33:44 +0200

    fix: SSW-1350 Umbenennung Fakturierung: Geschwisterkind innerhalb Stammdaten

--- Application/People/Person/Frontend/FrontendStudentGeneral.php
+++ Application/People/Person/Frontend/FrontendStudentGeneral.php
@@ -174,7 +174,7 @@ class FrontendStudentGeneral extends FrontendReadOnly
                 new LayoutRow(array(
                     new LayoutColumn(array(
                         FrontendReadOnly::getSubContent(
-                            'Fakturierung',
+                            'Geschwisterkind',
                             new Layout(new LayoutGroup(array(
                                 new LayoutRow(array(
                                     self::getLayoutColumnLabel('Geschwisterkind', 6),
@@ -416,7 +416,7 @@ class FrontendStudentGeneral extends FrontendReadOnly
             new FormGroup(array(
                 new FormRow(array(
                     new FormColumn(array(
-                        new Panel('Fakturierung', array(
+                        new Panel('Geschwisterkind', array(
                             new SelectBox('Meta[Billing]', 'Geschwisterkind', array('{{Name}}' => Relationship::useService()->getSiblingRankAll()),
                                 new Child()),
                         ), Panel::PANEL_TYPE_INFO),
