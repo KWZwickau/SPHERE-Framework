00fe5cae254c8f4b6e1552ad60d2e3d9905538d1
Kauschke <hannesk001@web.de>
2022-03-22 14:53:36 +0100

    fix: SSW-1644 SEKII Grunddaten Export für Untis analog Indiware

--- Application/Transfer/Untis/Export/Export.php
+++ Application/Transfer/Untis/Export/Export.php
@@ -3,7 +3,16 @@ namespace SPHERE\Application\Transfer\Untis\Export;
 
 use SPHERE\Application\IModuleInterface;
 use SPHERE\Application\IServiceInterface;
+use SPHERE\Common\Frontend\Icon\Repository\Listing;
 use SPHERE\Common\Frontend\IFrontendInterface;
+use SPHERE\Common\Frontend\Layout\Repository\Panel;
+use SPHERE\Common\Frontend\Layout\Repository\PullClear;
+use SPHERE\Common\Frontend\Layout\Structure\Layout;
+use SPHERE\Common\Frontend\Layout\Structure\LayoutColumn;
+use SPHERE\Common\Frontend\Layout\Structure\LayoutGroup;
+use SPHERE\Common\Frontend\Layout\Structure\LayoutRow;
+use SPHERE\Common\Frontend\Link\Repository\Standard;
+use SPHERE\Common\Frontend\Text\Repository\Center;
 use SPHERE\Common\Main;
 use SPHERE\Common\Window\Navigation\Link;
 use SPHERE\Common\Window\Stage;
@@ -50,6 +59,25 @@ class Export implements IModuleInterface
         $Stage = new Stage('Untis', 'Datentransfer');
 
         $Stage->setMessage('Daten exportieren');
+
+        $PanelMetaExport[] = new PullClear('Grunddaten für eine Klasse exportieren: '.
+            new Center(new Standard('', __NAMESPACE__.'/Meta', new Listing()
+                , array(), 'Auswahl der Klasse')));
+
+        $Stage->setMessage('Exportvorbereitung / Daten exportieren');
+
+        $Stage->setContent(
+            new Layout(
+                new LayoutGroup(
+                    new LayoutRow(array(
+                        new LayoutColumn(
+                            new Panel('Export der Grunddaten für SEK II', $PanelMetaExport
+                                , Panel::PANEL_TYPE_INFO)
+                            , 4),
+                    ))
+                )
+            )
+        );
 
         return $Stage;
     }
