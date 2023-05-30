74dc67bcbb6b65761fce9e407f58db611f157b64
Kauschke <hannesk001@web.de>
2023-05-30 11:41:45 +0200

    SSWHD-2163 FOS Klasse 12 Jahreszeugnis - Entfernung Versetzungsvermerk bei Klasse 12 für neues Kurssystem

--- Application/Api/Education/Certificate/Generator/Repository/HOGA/Style.php
+++ Application/Api/Education/Certificate/Generator/Repository/HOGA/Style.php
@@ -2474,10 +2474,7 @@ abstract class Style extends Certificate
     public function getCustomFosTransfer(int $personId, string $marginTop = '5px') : Slice
     {
         // SSWHD-2163 Klasse 12 bei den FOS Jahreszeugnissen besitzt keinen Versetzungsvermerk
-        if (($tblDivision = $this->getTblDivision())
-            && ($tblLevel = $tblDivision->getTblLevel())
-            && intval($tblLevel->getName()) == 12
-        ) {
+        if ($this->getLevel() == 12) {
             $content = '&nbsp;';
         } else {
             $content = '{% if(Content.P' . $personId . '.Input.Transfer) %}
