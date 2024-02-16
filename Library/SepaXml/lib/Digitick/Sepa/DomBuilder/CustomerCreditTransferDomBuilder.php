6762ba9414377187f57f8143ee2401bfbcf2f7c8
Rackel <siddy_88@gmx.de>
2024-02-16 10:58:58 +0100

    #SSWHD-2778 Fakturierung fehler beim Download SEPA

--- Library/SepaXml/lib/Digitick/Sepa/DomBuilder/CustomerCreditTransferDomBuilder.php
+++ Library/SepaXml/lib/Digitick/Sepa/DomBuilder/CustomerCreditTransferDomBuilder.php
@@ -183,11 +183,11 @@ class CustomerCreditTransferDomBuilder extends BaseDomBuilder
         $CdtTrfTxInf->appendChild($creditorAccount);
 
         // remittance 2.98 2.99
-        if (strlen($transactionInformation->getCreditorReference()) > 0)
+        if ($transactionInformation->getCreditorReference() && strlen($transactionInformation->getCreditorReference()) > 0)
         {
             $remittanceInformation = $this->getStructuredRemittanceElement($transactionInformation->getCreditorReference());
             $CdtTrfTxInf->appendChild($remittanceInformation);
-        } elseif (strlen($transactionInformation->getRemittanceInformation()) > 0) {
+        } elseif ($transactionInformation->getRemittanceInformation() && strlen($transactionInformation->getRemittanceInformation()) > 0) {
             $remittanceInformation = $this->getRemittenceElement($transactionInformation->getRemittanceInformation());
             $CdtTrfTxInf->appendChild($remittanceInformation);
         }
