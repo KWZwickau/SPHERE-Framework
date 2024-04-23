878f450d8bef47948aeade22e12074efeadaee67
Kauschke <hannesk001@web.de>
2024-04-23 07:35:55 +0200

    hotfix: SSWHD-2892 Shutdown Account: 17584 (22.04.2024 19:03:28)

--- Application/Api/Education/Certificate/Generator/Repository/MsAbsHsE.php
+++ Application/Api/Education/Certificate/Generator/Repository/MsAbsHsE.php
@@ -176,8 +176,8 @@ class MsAbsHsE extends Certificate
             ->addSlice($this->getDescriptionContent($personId, '200px', '15px'))
             ->addSlice($this->getDateLine($personId))
             ->addSlice((new MsAbsRs(
-                $this->getTblDivision() ? $this->getTblDivision() : null,
-                $this->getTblPrepareCertificate() ? $this->getTblPrepareCertificate() : null
+                $this->getTblStudentEducation() ?: null,
+                $this->getTblPrepareCertificate() ?: null
             ))->getExaminationsBoard('10px','11px'))
             ->addSlice($this->getInfo('170px',
                 'Notenerläuterung:',
