c6b3e10442e9c49251f11024b50c2abdad4e040b
Kauschke <hannesk001@web.de>
2025-02-10 13:17:57 +0100

    hotfix: SSWHD-3318 Zeugniserstellung Berufl. Gymnasium Montessori

--- Application/Api/Education/Certificate/Generator/Repository/BGymJ.php
+++ Application/Api/Education/Certificate/Generator/Repository/BGymJ.php
@@ -4,6 +4,8 @@ namespace SPHERE\Application\Api\Education\Certificate\Generator\Repository;
 
 use SPHERE\Application\Education\Certificate\Generator\Repository\Page;
 use SPHERE\Application\People\Person\Service\Entity\TblPerson;
+use SPHERE\Application\Platform\Gatekeeper\Authorization\Consumer\Consumer as GatekeeperConsumer;
+use SPHERE\Application\Platform\Gatekeeper\Authorization\Consumer\Service\Entity\TblConsumer;
 
 class BGymJ extends BGymStyle
 {
@@ -27,13 +29,15 @@ class BGymJ extends BGymStyle
     {
         $personId = $tblPerson ? $tblPerson->getId() : 0;
 
+        $isMissing = GatekeeperConsumer::useService()->getConsumerBySessionIsConsumer(TblConsumer::TYPE_SACHSEN, 'EMSP');
+
         return (new Page())
             ->addSlice($this->getHeaderBGym('Jahreszeugnis'))
             ->addSlice($this->getSubjectArea($personId))
             ->addSlice($this->getLevelYearStudent($personId, 'Schuljahr'))
             ->addSlice($this->getSubjectLanesBGym($personId, 'Schuljahr'))
             ->addSlice($this->getChosenLanesBGym($personId))
-            ->addSlice($this->getRemarkBGym($personId, false))
+            ->addSlice($this->getRemarkBGym($personId, $isMissing))
             ->addSlice($this->getTransferBGym($personId))
             ->addSlice($this->getSignPartBGym($personId))
             ->addSlice($this->getGradeLevel())
