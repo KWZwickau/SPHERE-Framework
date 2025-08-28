af80c74456c83601410de0c50ee60a3f29a1098b
Kauschke <hannesk001@web.de>
2025-08-28 09:24:48 +0200

    hotfix: SSWHD-3665 EZSH - Stundenplan - Anpassung Fehlermeldung fehlende Klassen

--- Application/Transfer/Indiware/Import/Timetable/TimetableService.php
+++ Application/Transfer/Indiware/Import/Timetable/TimetableService.php
@@ -401,12 +401,14 @@ class TimetableService
                             $tblDivisionCourse = Education::useService()->getDivisionCourseByDivisionNameAndYear($Row['Course'], $tblYear);
                         }
 
-                        if ($tblDivisionCourse && $tblSubject) {
+                        if ($tblDivisionCourse) {
                             // Spezialfall: Stundenplan für SekII -> es werden direkt beim Stundenplan die SekII-Kurse zugeordnet, falls vorhanden
                             if (DivisionCourse::useService()->getIsCourseSystemByStudentsInDivisionCourse($tblDivisionCourse)
                                 && ($tblStudentList = $tblDivisionCourse->getStudents())
                                 && ($tblYear = $tblDivisionCourse->getServiceTblYear())
+                                && $tblSubject
                             ) {
+                                $tblDivisionCourse = false;
                                 foreach ($tblStudentList as $tblStudent) {
                                     if (($tblStudentEducation = DivisionCourse::useService()->getStudentEducationByPersonAndYear($tblStudent, $tblYear))
                                         && ($level = $tblStudentEducation->getLevel())
